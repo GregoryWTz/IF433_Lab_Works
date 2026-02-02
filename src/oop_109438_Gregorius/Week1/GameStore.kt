fa27f331ee782a92db3ea4fcdaf36b6c6981e21f
GregoryWTz <gwtlibra@gmail.com>
2026-02-02 16:48:27 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_109438_Gregorius/Week1/GameStore.kt
+++ src/oop_109438_Gregorius/Week1/GameStore.kt
@@ -3,4 +3,7 @@ package oop_109438_Gregorius.Week1
 fun main() {
     val gameTitle: String = "God Of War"
     val price: Int = 700000
+    val newPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) price * 0.8 else price * 0.9
