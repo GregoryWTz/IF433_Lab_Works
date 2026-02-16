02c580edb2a7a5c9b149fc9665eab9c666be6e7a
GregoryWTz <gwtlibra@gmail.com>
2026-02-16 15:10:09 +0700

    week03: add private property

--- src/oop_109438_Gregorius/Week3/Employee.kt
+++ src/oop_109438_Gregorius/Week3/Employee.kt
@@ -5,9 +5,23 @@ class Employee(val name: String) {
     set(value) {
         if (value < 0) {
             println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-            field = 0
+            field = 0 // Gunakan field, bukan this.salary
         } else {
-            field = value
+            field = value // Gunakan field untuk assign nilai asli
         }
+    }
+
+    // Hanya bisa diakses di file/class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar raha
+    // kecuali kita buat function khusus untuk print
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
     }
 }
