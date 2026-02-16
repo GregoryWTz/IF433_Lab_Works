6acaa9d0b3d58f6bee1fd91f4ed52913df5e0024
GregoryWTz <gwtlibra@gmail.com>
2026-02-16 15:32:54 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_109438_Gregorius/Week3/TugasMandiri1/Weapon.kt
@@ -0,0 +1,22 @@
+package oop_109438_Gregorius.Week3.TugasMandiri1
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai tetap $field.")
+            } else if (value > 1000) {
+                println("Peringatan: Weapon Overpowered! Damage dibatasi ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
