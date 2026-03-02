c1d8e28b4b81cde3955524096d529683dd338f63
GregoryWTz <gwtlibra@gmail.com>
2026-03-02 13:28:04 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_109438_Gregorius/Week5/TugasMandiri1/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_109438_Gregorius.Week5.TugasMandiri1
+
+class MathHelper {
+    fun hitungLuas(sisi: Int) : Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int) : Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jarijari: Double) : Double {
+        return 3.14 * jarijari * jarijari
+    }
+}
