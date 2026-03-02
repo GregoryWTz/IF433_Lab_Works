ca3de0805c89272ed235dc2b889f2af3069a1114
GregoryWTz <gwtlibra@gmail.com>
2026-03-02 13:02:42 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_109438_Gregorius/Week5/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_109438_Gregorius.Week5
+
+abstract class Pegawai(val name: String) {
+    // Abstract function tidak memiliki body body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
