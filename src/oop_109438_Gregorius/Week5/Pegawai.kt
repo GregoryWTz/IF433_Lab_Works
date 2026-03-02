df430f675a69a1f96e1852413ad47e754bfeea93
GregoryWTz <gwtlibra@gmail.com>
2026-03-02 13:06:37 +0700

    week05: create Dosen class inheriting Pegawai

--- src/oop_109438_Gregorius/Week5/Pegawai.kt
+++ src/oop_109438_Gregorius/Week5/Pegawai.kt
@@ -1,6 +1,6 @@
 package oop_109438_Gregorius.Week5
 
-abstract class Pegawai(val name: String) {
+abstract class Pegawai(val nama: String) {
     // Abstract function tidak memiliki body body (tanpa kurung kurawal)
     abstract fun bekerja()
 }
