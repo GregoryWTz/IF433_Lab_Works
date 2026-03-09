33439f06164aada402232ee8c60d4f04cc7f8b1b
GregoryWTz <gwtlibra@gmail.com>
2026-03-09 13:01:09 +0700

    week06: create compound interfaces

---
+++ src/oop_109438_Gregorius/Week6/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_109438_Gregorius.Week6
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
