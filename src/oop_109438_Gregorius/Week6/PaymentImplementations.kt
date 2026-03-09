98c8bbb65a69b95406534a7af26c90bf749db1a3
GregoryWTz <gwtlibra@gmail.com>
2026-03-09 13:15:33 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_109438_Gregorius/Week6/PaymentImplementations.kt
@@ -0,0 +1,13 @@
+package oop_109438_Gregorius.Week6
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
+}
