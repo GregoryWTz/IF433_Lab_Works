17f6eda2e258b44ec3aff9131cb7ff5bf9c92252
GregoryWTz <gwtlibra@gmail.com>
2026-03-09 13:06:34 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_109438_Gregorius/Week6/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_109438_Gregorius.Week6
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
