7cb0a701317345f6e46ce5e813c111e84f63798c
GregoryWTz <gwtlibra@gmail.com>
2026-03-16 15:23:42 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_109438_Gregorius/Week7/TugasMandiri/Main.kt
+++ src/oop_109438_Gregorius/Week7/TugasMandiri/Main.kt
@@ -1,6 +1,17 @@
 package oop_109438_Gregorius.Week7.TugasMandiri
 
 fun main() {
+    // Step 7 - Singleton
     GameManager.startGame()
-    GameManager.startGame() // Test untuk kedua kali nya
+    GameManager.startGame()
+
+    // Step 8 - Factory & Enum
+    println("\n--- Simulasi Factory & Enum ---")
+
+    // Cetak drop chance LEGENDARY
+    println("Drop Chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    // Buat senjata awal via Factory
+    val starterSword = Weapon.forgeStarterSword()
+    println("Senjata Awal: ${starterSword.item.name} | Damage: ${starterSword.item.damage} | Rarity: ${starterSword.item.rarity} | Durability: ${starterSword.durability}")
 }
