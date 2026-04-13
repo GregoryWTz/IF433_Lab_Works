57c0435e69e14c73c59cb7850115ad0844198557
GregoryWTz <gwtlibra@gmail.com>
2026-04-13 14:54:13 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_109438_Gregorius/Week8/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_109438_Gregorius.Week8;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembalian Nullable
+    prublic static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK"
+    }
+}
