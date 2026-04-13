c3a99b455ac4aab314d7668bd2faeea8f58a98df
GregoryWTz <gwtlibra@gmail.com>
2026-04-13 15:11:35 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_109438_Gregorius/Week8/TugasMandiri1/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_109438_Gregorius.Week8.TugasMandiri1;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
