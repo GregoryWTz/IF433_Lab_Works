e46dcc3e73284169567bae10d34266620144a19b
GregoryWTz <gwtlibra@gmail.com>
2026-04-27 14:11:47 +0700

    week10: (task) populate coin repository

--- src/oop_109438_Gregorius/Week10/TugasMandiri1/CryptoDashboard.kt
+++ src/oop_109438_Gregorius/Week10/TugasMandiri1/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_109438_Gregorius.Week10.TugasMandiri1
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin( "BTC", 65000.0))
+    coinRepo.add(Coin( "ETH", 3500.0))
+    coinRepo.add(Coin( "SOL", 140.0))
 }
