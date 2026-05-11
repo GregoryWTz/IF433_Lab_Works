4517d1684067b4900f5a5f6297fe65e2ce7a05ea
GregoryWTz <gwtlibra@gmail.com>
2026-05-11 15:30:53 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_109438_Gregorius/Week12/TugasMandiri/SmartFeeder.kt
+++ src/oop_109438_Gregorius/Week12/TugasMandiri/SmartFeeder.kt
@@ -2,5 +2,13 @@ package oop_109438_Gregorius.Week12.TugasMandiri
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-    return if (isJammed) throw DispenserJamException() else availableGram - requestedGram
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    return availableGram - requestedGram
 }
