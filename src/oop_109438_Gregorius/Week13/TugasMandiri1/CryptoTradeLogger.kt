f020aced6a536922039d38763bd4d5d20f880b88
GregoryWTz <gwtlibra@gmail.com>
2026-05-18 13:19:07 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_109438_Gregorius/Week13/TugasMandiri1/CryptoTradeLogger.kt
+++ src/oop_109438_Gregorius/Week13/TugasMandiri1/CryptoTradeLogger.kt
@@ -1,3 +1,8 @@
 package oop_109438_Gregorius.Week13.TugasMandiri1
+import java.io.File
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+fun TradeRecord.toCsv(): String {
+    return "$id,$symbol,$type,$margin,$pnl"
+}
